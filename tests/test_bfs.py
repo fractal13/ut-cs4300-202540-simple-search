dc99e1e84a30b92b5fdb16213675d4c07107d7a9
Curtis Larsen <curtis.gale.larsen@gmail.com>
2025-08-29 14:30:25 -0600

    Add BFS test for states one step from goal

--- tests/test_bfs.py
+++ tests/test_bfs.py
@@ -13,3 +13,23 @@ def test_bfs_goal_start():
     prob = WolfGoatCabbageProblem(start=goal, goal=goal)
     path = bfs(prob)
     assert path == [(goal, None)]
+
+
+def test_bfs_one_step_from_goal():
+    goal = WolfGoatCabbageState(False, False, False, False)
+    # generate states one action away from goal by reversing transitions
+    prob = WolfGoatCabbageProblem(goal=goal)
+    one_step_states = []
+    for s_bits in __import__('itertools').product([False, True], repeat=4):
+        s = WolfGoatCabbageState(*s_bits)
+        for a in prob.Actions(s):
+            if prob.Transition(s, a) == goal:
+                one_step_states.append((s, a))
+    assert len(one_step_states) > 0
+    # test BFS returns path of length 2 (start->goal) for each such state
+    for s, a in one_step_states:
+        prob2 = WolfGoatCabbageProblem(start=s, goal=goal)
+        path = bfs(prob2)
+        assert len(path) == 2
+        assert path[0][0] == s and path[0][1] is None
+        assert path[1][0] == goal and path[1][1] == a
